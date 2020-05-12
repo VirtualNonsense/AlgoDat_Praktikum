4fdf5c6089fef6a5ca4bb515c19f3efcc9d27369
VirtualNonsense <virtualnonsense@mailbox.org>
2020-05-12 18:23:39 +0200

    Initialized file structure

---
+++ AlgoDatDictionaries/IDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoDatDictionaries
+{
+    interface IDictionary
+    {
+        bool Search();
+        bool Insert();
+        bool Delete();
+        void Print();
+    }
+}
