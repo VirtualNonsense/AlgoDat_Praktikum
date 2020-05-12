368396ef3f8fcb5d642a0d6507f8c0a43a3e55d6
VirtualNonsense <virtualnonsense@mailbox.org>
2020-05-12 16:02:14 +0200

    initial commit

---
+++ AlgoDatConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoDatConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
