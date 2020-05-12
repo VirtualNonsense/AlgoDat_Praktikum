bad762389c8f2e8ee2ecefb7d58c6c3501924487
VirtualNonsense <virtualnonsense@mailbox.org>
2020-05-12 18:24:18 +0200

    Added baseclass for arrays

---
+++ AlgoDatDictionaries/Arrays/ServiceArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoDatDictionaries.Arrays
+{
+    public abstract class ServiceArray
+    {
+        protected int[] array; 
+        
+        public void Print()
+        {
+            foreach(int i in array)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
