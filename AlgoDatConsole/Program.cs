31a67c1a90565d3552dd385650e586475e8fe90a
Lucas <kirchbachlu74675@th-nuernberg.de>
2020-05-12 18:34:58 +0200

    Added a cw to main

--- AlgoDatConsole/Program.cs
+++ AlgoDatConsole/Program.cs
@@ -7,6 +7,8 @@ namespace AlgoDatConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Lucas war hier");
         }
     }
 }
