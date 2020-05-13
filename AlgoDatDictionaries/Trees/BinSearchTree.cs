c74c5a5eb83f2b167e7866bd2184e8cc6481494d
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-05-13 17:32:53 +0200

    added baseclasses

---
+++ AlgoDatDictionaries/Trees/BinSearchTree.cs
@@ -0,0 +1,43 @@
+﻿namespace AlgoDatDictionaries.Trees
+{
+    public class BinSearchTree : ISetSorted
+    {
+        private TreeNode root;
+        
+        // ###############################################
+        // Constructor
+        // ###############################################
+        public BinSearchTree()
+        {
+            
+        }
+        
+        
+        // ###############################################
+        // ISetSorted
+        // ###############################################
+        public bool Search()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Insert()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        // ###############################################
+        // Private Stuff
+        // ###############################################
+    }
+}
---
+++ AlgoDatDictionaries/Trees/TreeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoDatDictionaries.Trees
+{
+    public class TreeNode
+    {
+        protected int value;
+
+        protected TreeNode left;
+        protected TreeNode right;
+    }
+}
