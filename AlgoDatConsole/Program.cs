978aa3806ded9b507ee7a46348020de068e115c5
KiLucas <kirchbachlu74675@th-nuernberg.de>
2020-05-13 19:04:06 +0200

    tried to change the program on monodevelop IDE and commit from linux distro

--- AlgoDatConsole/Program.cs
+++ AlgoDatConsole/Program.cs
@@ -8,7 +8,8 @@ namespace AlgoDatConsole
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Lucas war hier");
+            Console.WriteLine("Lucas war hier über Monodevelop");
+            Console.WriteLine("nur n testversuch für linux");
         }
     }
 }
