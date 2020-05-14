3e1de1ca86e8b829f02f18744b3d130bbd82e527
Lucas <kirchbachlu74675@th-nuernberg.de>
2020-05-14 12:56:07 +0200

    Folder Lists was missing > added

---
+++ AlgoDatDictionaries/Lists/llnode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoDatDictionaries.Lists
+{
+    public class llnode
+    {
+        int key;
+        llnode next;
+
+
+        // Properties
+
+        public int Key
+        { 
+            get { return key; }
+            set { key = value; }
+        }
+
+        public llnode Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        // Constructors
+
+        public llnode() 
+        {
+            // Default constructor
+        }
+
+        public llnode(int key, llnode next)
+        {
+            this.key = key;
+            this.next = next;
+        }
+    }
+}
