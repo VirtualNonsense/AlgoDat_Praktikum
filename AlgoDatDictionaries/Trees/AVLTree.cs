328c30da8ba3b6c9f7f32a4f37869589bc6556cc
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-05-21 14:50:50 +0200

    First draft of GetUnbalancedNode Method

--- AlgoDatDictionaries/Trees/AVLTree.cs
+++ AlgoDatDictionaries/Trees/AVLTree.cs
@@ -10,7 +10,7 @@ namespace AlgoDatDictionaries.Trees
     public class AVLTree : BinSearchTree
     {
         private readonly int _balanceThreshold = 2;
-        private bool _balance = true;
+        private bool _enableBalance = true;
 
         public AVLTree()
         {
@@ -21,32 +21,59 @@ namespace AlgoDatDictionaries.Trees
         /// Used for testing
         /// When balance is false the insert and delete method will behave like on the parent class.
         /// </summary>
-        /// <param name="balance"></param>
-        internal AVLTree(bool balance)
+        /// <param name="enableBalance"></param>
+        internal AVLTree(bool enableBalance)
         {
-            _balance = balance;
+            _enableBalance = enableBalance;
         }
         public override bool Insert(int value)
         {
             var search = base.search(value);
             var result = Insert(search, value);
-            if(result && _balance)
-                Balance();
-            return result;
+            if (!result || !_enableBalance) return result;
+            var (treeNode, balance) = GetUnbalancedNode(value);
+            Balance(treeNode, balance);
+            return true;
         }
 
         public override bool Delete(int value)
         {
             var search = base.search(value);
             var result = Delete(search, value);
-            if(result && _balance)
-                Balance();
-            return result;
+            if (!result || !_enableBalance) return result;
+            var (treeNode, balance) = GetUnbalancedNode(value);
+            Balance(treeNode, balance);
+            return true;
         }
 
-        private void Balance()
+        private void Balance(TreeNode node, int balance)
         {
-            throw new NotImplementedException();
+            
+        }
+
+        /// <summary>
+        /// This function will search similar to search for the given value but will return the first unbalanced node
+        /// instead of prenode node directon and found.
+        /// </summary>
+        /// <param name="value"></param>
+        internal (TreeNode, int) GetUnbalancedNode(int value)
+        {
+            if (root == null)
+                return (null, 0);
+            var a = root;                
+            var balance = a.Balance;
+            while(a != null && a.Value != value && Math.Abs(balance) < _balanceThreshold)
+            {
+                if (value < a.Value)
+                {
+                    a = a.Left;
+                    balance = a.Balance;
+                    continue;
+                }
+                a = a.Right;
+                balance = a.Balance;
+            }
+            return (a, balance);
         }
 
         
