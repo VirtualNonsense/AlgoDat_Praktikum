affc61730f6b4d343b3a8abd4b130ee4cf098428
Lucas <kirchbachlu74675@th-nuernberg.de>
2020-05-22 19:17:34 +0200

    started developing a menu

---
+++ AlgoDatConsole/Menu.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AlgoDatConsole
+{
+    class Menu
+    {
+        public static void PrintBanner()
+        {
+            Console.WriteLine(@"  _____  _      _   _                        _");
+            Console.WriteLine(@" |  __ \(_)    | | (_)                      (_)          ");
+            Console.WriteLine(@" | |  | |_  ___| |_ _  ___  _ __   __ _ _ __ _  ___  ___ ");
+            Console.WriteLine(@" | |  | | |/ __| __| |/ _ \| '_ \ / _` | '__| |/ _ \/ __|");
+            Console.WriteLine(@" | |__| | | (__| |_| | (_) | | | | (_| | |  | |  __/\__ \");
+            Console.WriteLine(@" |_____/|_|\___|\__|_|\___/|_| |_|\__,_|_|  |_|\___||___/");
+            Console.WriteLine();
+        }
+
+        public static void Printbanner1()
+        {
+            Console.WriteLine(@"                                ");
+            Console.WriteLine(@"     /\                         ");
+            Console.WriteLine(@"    /  \   _ __ _ __ __ _ _   _ ");
+            Console.WriteLine(@"   / /\ \ | '__| '__/ _` | | | |");
+            Console.WriteLine(@"  / ____ \| |  | | | (_| | |_| |");
+            Console.WriteLine(@" /_/    \_\_|  |_|  \__,_|\__, |");
+            Console.WriteLine(@"                           __/ |");
+            Console.WriteLine(@"                          |___/ ");
+            Console.WriteLine();
+        }
+
+        public static void Printbanner2()
+        {
+
+        }
+        public static void Printbanner3()
+        {
+
+        }
+        public static void Printbanner4()
+        {
+
+        }
+        public static void Printbanner5()
+        {
+
+        }
+        public static void PrintSuggestions()
+        {
+            Console.WriteLine("Press 1 for Array program");
+            Console.WriteLine("Press 2 for Lists program");
+            Console.WriteLine("Press 3 for Binary Tree program");
+            Console.WriteLine("Press 4 for AVL Tree program");
+            Console.WriteLine("Press 5 for Treaps program");
+            Console.WriteLine("Press 6 for Hash Algorithm" );
+        }
+
+
+
+    }
+}
--- AlgoDatConsole/Program.cs
+++ AlgoDatConsole/Program.cs
@@ -9,171 +9,64 @@ namespace AlgoDatConsole
     {
         static void Main(string[] args)
         {
-            // Sorry Leute ich will ne Ausgabe
-            // Können wir verschieben in die Tests
+            ConsoleKeyInfo input;
 
-            // Check MultiSetUnsorted
-            Console.WriteLine("MultiSetUnsorted:");
-            IDictionary msu = new MultiSetUnsortedLinkedList();
+            do // Outer Loop - Dictionary
+            {
+                Console.Clear();
+                Menu.PrintBanner();
+                Menu.PrintSuggestions();
+                input = Console.ReadKey();
+                if (input.Key == ConsoleKey.D1)
+                {
+                    do // Inner Loop Array
+                    {
 
-            Console.Write(msu.Insert(400) + " ");
-            Console.Write(msu.Insert(1) + " ");
-            Console.Write(msu.Insert(50) + " ");
-            Console.Write(msu.Insert(2) + " ");
-            Console.Write(msu.Insert(869) + " ");
-            Console.Write(msu.Insert(400) + " ");
-            Console.WriteLine(msu.Insert(2));
+                    } while (input.Key != ConsoleKey.Backspace);
 
-            msu.Print();
+                }
+                if (input.Key == ConsoleKey.D2)
+                {
+                    do // Inner Loop Lists
+                    { 
 
-            Console.Write(msu.Search(400) + " ");
-            Console.Write(msu.Search(1) + " ");
-            Console.WriteLine(msu.Search(800));
+                    } while (input.Key != ConsoleKey.Backspace);
+                }
+                if (input.Key == ConsoleKey.D3)
+                {
+                    do // Inner Loop Binary Search Tree
+                    {
 
-            Console.Write(msu.Delete(400) + " ");
-            Console.Write(msu.Delete(400) + " ");
-            Console.Write(msu.Delete(1) + " ");
-            Console.WriteLine(msu.Delete(500) + " ");
+                    } while (input.Key != ConsoleKey.Backspace);
+                }
+                if (input.Key == ConsoleKey.D4)
+                {
+                    do // Inner Loop AVL
+                    {
 
-            msu.Print();
-            Console.WriteLine();
+                    } while (input.Key != ConsoleKey.Backspace);
+                }
+                if (input.Key == ConsoleKey.D5)
+                {
+                    do // Inner Loop Treap
+                    {
 
-            // Check MultiSetSorted
-            Console.WriteLine("MultiSetSorted:");
-            IDictionary mss = new MultiSetSortedLinkedList();
+                    } while (input.Key != ConsoleKey.Backspace);
+                }
+                if (input.Key == ConsoleKey.D6)
+                {
+                    do // Inner Loop Hash
+                    {
 
-            Console.Write(mss.Insert(400) + " ");
-            Console.Write(mss.Insert(2) + " ");
-            Console.Write(mss.Insert(50) + " ");
-            Console.Write(mss.Insert(1) + " ");
-            Console.Write(mss.Insert(869) + " ");
-            Console.Write(mss.Insert(400) + " ");
-            Console.WriteLine(mss.Insert(2));
-
-            mss.Print();
-
-            Console.Write(mss.Search(400) + " ");
-            Console.Write(mss.Search(1) + " ");
-            Console.Write(mss.Search(800) + " ");
-            Console.WriteLine(mss.Search(869));
-
-            Console.Write(mss.Delete(400) + " ");
-            Console.Write(mss.Delete(400) + " ");
-            Console.Write(mss.Delete(1) + " ");
-            Console.Write(mss.Delete(400) + " ");
-            Console.Write(mss.Delete(859) + " ");
-            Console.WriteLine(mss.Delete(500) + " ");
-
-            mss.Print();
-            Console.WriteLine();
-
-            // Check SetUnsorted
-            Console.WriteLine("SetUnsorted:");
-            IDictionary su = new SetUnsortedLinkedList();
-
-            Console.Write(su.Insert(400) + " ");
-            Console.Write(su.Insert(2) + " ");
-            Console.Write(su.Insert(50) + " ");
-            Console.Write(su.Insert(1) + " ");
-            Console.Write(su.Insert(869) + " ");
-            Console.Write(su.Insert(400) + " ");
-            Console.WriteLine(su.Insert(2));
-
-            su.Print();
-
-            Console.Write(su.Search(400) + " ");
-            Console.Write(su.Search(1) + " ");
-            Console.Write(su.Search(800) + " ");
-            Console.WriteLine(su.Search(869));
-
-            Console.Write(su.Delete(400) + " ");
-            Console.Write(su.Delete(400) + " ");
-            Console.Write(su.Delete(1) + " ");
-            Console.Write(su.Delete(400) + " ");
-            Console.Write(su.Delete(959) + " ");
-            Console.Write(su.Delete(500) + " ");
-            Console.WriteLine(su.Delete(869) + " ");
-
-            su.Print();
-            Console.WriteLine();
-
-            // Check SetUnsorted
-            Console.WriteLine("SetSorted:");
-            IDictionary ss = new SetSortedLinkedList();
-
-            Console.Write(ss.Insert(400) + " ");
-            Console.Write(ss.Insert(2) + " ");
-            Console.Write(ss.Insert(50) + " ");
-            Console.Write(ss.Insert(50) + " ");
-            Console.Write(ss.Insert(1) + " ");
-            Console.Write(ss.Insert(869) + " ");
-            Console.Write(ss.Insert(400) + " ");
-            Console.WriteLine(ss.Insert(2));
-
-            ss.Print();
-
-            Console.Write(ss.Search(400) + " ");
-            Console.Write(ss.Search(1) + " ");
-            Console.Write(ss.Search(800) + " ");
-            Console.WriteLine(ss.Search(869));
-
-            Console.Write(ss.Delete(400) + " ");
-            Console.Write(ss.Delete(400) + " ");
-            Console.Write(ss.Delete(1) + " ");
-            Console.Write(ss.Delete(400) + " ");
-            Console.Write(ss.Delete(959) + " ");
-            Console.Write(ss.Delete(500) + " ");
-            Console.WriteLine(ss.Delete(869) + " ");
-
-            ss.Print();
-            Console.WriteLine();
-
-            IDictionary dummy = new MultiSetSortedLinkedList();
-            dummy.Delete(5);
-            dummy.Print();
+                    } while (input.Key != ConsoleKey.Backspace);
+                }
 
 
-            ////bin zu dämlich für die unit tests
-            //int[] nums = { 1, 2, 3, 4, 4, 5, };
-            //MultiSetSortedLinkedList testlist = new MultiSetSortedLinkedList();
-            //foreach (int  item in nums)
-            //{
-            //    testlist.Insert(item);
-            //}
-            //testlist.Print();
+                //input = Console.ReadKey();
 
-            //Console.WriteLine("deleting root");
-            //testlist.Delete(1);
-            //testlist.Print();
+            } while (input.Key != ConsoleKey.Escape);
 
-            //Console.WriteLine("deleting inbetween");
-            //testlist.Delete(4);
-
-            //testlist.Print();
-
-            //Console.WriteLine("deleting last");
-            //testlist.Delete(5);
-
-            //testlist.Print();
-
-            //SetUnSortedLinkedList setun = new SetUnSortedLinkedList();
-            //setun.Insert(100);
-
-            //setun.Print();
-
-            //IDictionary listone = new SetUnSortedLinkedList();
-            //listone.Insert(200);
-            //listone.Insert(100);
-            //listone.Insert(10);
-            //listone.Insert(99);
-            //listone.Insert(50);
-            //listone.Insert(20);
-
-            //listone.Print();
-
-            //listone.Insert(10);
-
-            //listone.Print();
+            
         }
     }
 }
