9e598e715d8980ac76026ccc19ae2ce43bd64183
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-05-28 17:37:22 +0200

    Right and Left of TreapNode will cast correctly.

--- AlgoDatDictionaries/Trees/TreapNode.cs
+++ AlgoDatDictionaries/Trees/TreapNode.cs
@@ -3,6 +3,19 @@
     public class TreapNode : BinSearchTreeNode
     {
         public int Priority { get; }
+        
+        
+        public new TreapNode Right
+        {
+            get => (TreapNode) base.Right;
+            set => base.Right = value;
+        }
+        
+        public new TreapNode Left
+        {
+            get => (TreapNode) base.Left;
+            set => base.Left = value;
+        }
 
         public TreapNode(int value, int prio) : base(value)
         {
