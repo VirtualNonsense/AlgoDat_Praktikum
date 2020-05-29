f7b1042ba3642a45f7171b3132f19bacd2b70abe
khang buivan <khang-buivan@hotmail.de>
2020-05-29 16:32:50 +0200

    implemented insert method and TreapTreetest baseclass

--- AlgoDatDictionaries/Trees/Treap.cs
+++ AlgoDatDictionaries/Trees/Treap.cs
@@ -6,10 +6,80 @@ namespace AlgoDatDictionaries.Trees
     public class Treap : BinSearchTree
     {
         private Random _random = new System.Random();
+        protected new TreapNode Root
+        {
+            get => (TreapNode)base.Root;
+            set => base.Root = value;
+        }
+
         public override bool Insert(int value)
         {
-            throw new NotImplementedException();
+            return Insert(value, _random.Next(1,1001));
         }
+        internal bool Insert(int value, int prio)
+        {
+            // Search vor insert node pre
+            var (prePre, pre, _, dir, _) = EvenMoreDetailedSearch(value);
+
+            // Perform insert
+            var result = Insert(pre, dir, value, prio);
+
+            // Step out if
+            if (!result // Insert was unsuccessful
+                || dir == Direction.Unset) // Root was inserted 
+                return result;
+
+            //// Get inserted node
+            //var node = pre == null ? Root : (dir == Direction.Left ? pre.Left : pre.Right);>
+
+            //Heap condition
+            CheckHeap(value);
+
+            // Insert successful
+            return true;
+        }
+  
+        internal virtual bool Insert(TreapNode pre, Direction dir, int value, int priority)
+        {
+            switch (dir)
+            {
+                case Direction.Unset:
+                    if (Root != null) return false;
+                    Root = new TreapNode(value, priority);
+                    break;
+                case Direction.Left:
+                    if (pre.Left != null) return false;
+                    pre.Left = new TreapNode(value, priority);
+                    break;
+                case Direction.Right:
+                    if (pre.Right != null) return false;
+                    pre.Right = new TreapNode(value, priority);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(dir), dir, null);
+            }
+            return true;
+        }
+
+
+        public void CheckHeap(int value)
+        {
+            var (prePre, pre, node, dir, _) = EvenMoreDetailedSearch(value);
+
+            if (pre.Priority <= node.Priority)
+                return;
+            else
+            {
+                if (dir == Direction.Left)
+                    TurnRight(prePre, pre, node, dir);
+                else
+                    TurnLeft(prePre, pre, node, dir);
+
+                CheckHeap(value);
+            }     
+        }
+
+
         public override bool Delete(int value)
         {
             throw new NotImplementedException();
@@ -34,6 +104,11 @@ namespace AlgoDatDictionaries.Trees
         {
             var n = (TreapNode) node;
             return base.IntendPrint($"{n.Value},{n.Priority}", intend, endOfLine, dir);
+        }
+        internal new(TreapNode, TreapNode, TreapNode, Direction, bool) EvenMoreDetailedSearch(int value)
+        {
+            var (prePre, pre, node, dir, found) = base.EvenMoreDetailedSearch(value);
+            return ((TreapNode)prePre, (TreapNode)pre, (TreapNode)node, dir, found);
         }
     }
 }
---
+++ tests/SearchTrees/TreapTreeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AlgoDatDictionaries.Lists;
+using AlgoDatDictionaries.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests.SearchTrees
+{
+    [TestClass]
+    public class TreapTreeTest
+    {
+        [TestMethod]
+
+        public void Inserttest()
+        {
+
+        }
+
+    }
+}
