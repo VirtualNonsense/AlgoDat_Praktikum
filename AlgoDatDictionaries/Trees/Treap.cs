e154ec42cd593e06d339722cd45b651900fe29c8
jakobblohm <jakobblohm@web.de>
2020-05-30 14:35:46 +0200

    Delete method + DownHeap implemented adjusted insert method for random priority only

--- AlgoDatDictionaries/Trees/Treap.cs
+++ AlgoDatDictionaries/Trees/Treap.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Text;
 namespace AlgoDatDictionaries.Trees
@@ -12,63 +13,46 @@ namespace AlgoDatDictionaries.Trees
             set => base.Root = value;
         }
 
-        public override bool Insert(int value)
+        internal new(TreapNode, TreapNode, TreapNode, Direction, bool) EvenMoreDetailedSearch(int value)
         {
-            return Insert(value, _random.Next(1,1001));
+            var (prePre, pre, node, dir, found) = base.EvenMoreDetailedSearch(value);
+            return ((TreapNode)prePre, (TreapNode)pre, (TreapNode)node, dir, found);
         }
-        internal bool Insert(int value, int prio)
+
+
+        // Insert methods
+
+        // Insert method for random priority
+        public override bool Insert(int value)
         {
             // Search vor insert node pre
             var (prePre, pre, _, dir, _) = EvenMoreDetailedSearch(value);
 
             // Perform insert
-            var result = Insert(pre, dir, value, prio);
+            var result = Insert(pre, dir, value);
 
             // Step out if
             if (!result // Insert was unsuccessful
                 || dir == Direction.Unset) // Root was inserted 
                 return result;
 
-            //// Get inserted node
-            //var node = pre == null ? Root : (dir == Direction.Left ? pre.Left : pre.Right);>
+            // Get inserted node
+            var node = pre == null ? Root : (dir == Direction.Left ? pre.Left : pre.Right);>
 
-            //Heap condition
+            // Check Heap condition
             CheckHeap(value);
 
             // Insert successful
             return true;
         }
-  
-        internal virtual bool Insert(TreapNode pre, Direction dir, int value, int priority)
-        {
-            switch (dir)
-            {
-                case Direction.Unset:
-                    if (Root != null) return false;
-                    Root = new TreapNode(value, priority);
-                    break;
-                case Direction.Left:
-                    if (pre.Left != null) return false;
-                    pre.Left = new TreapNode(value, priority);
-                    break;
-                case Direction.Right:
-                    if (pre.Right != null) return false;
-                    pre.Right = new TreapNode(value, priority);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(dir), dir, null);
-            }
-            return true;
-        }
 
 
+        // Recursive method that rotates the node until the heap condition is met
         public void CheckHeap(int value)
         {
             var (prePre, pre, node, dir, _) = EvenMoreDetailedSearch(value);
 
-            if (pre.Priority <= node.Priority)
-                return;
-            else
+            if(pre.Priority > node.Priority)
             {
                 if (dir == Direction.Left)
                     TurnRight(prePre, pre, node, dir);
@@ -79,36 +63,79 @@ namespace AlgoDatDictionaries.Trees
             }     
         }
 
-
-        public override bool Delete(int value)
-        {
-            throw new NotImplementedException();
-        }
-        
         protected override BinSearchTreeNode ConstructTreeNode(int value)
         {
             return new TreapNode(value, _random.Next());
         }
 
-        public void DownHeap(BinSearchTreeNode node)
+
+        // Delete methods
+
+        // Delete method
+        public override bool Delete(int value)
         {
-            throw new NotImplementedException();
+            // Case 1: node with given value does not exist => nothing can be done
+            if (!Search(value))
+                return false;
+
+            // Case 2: Node with given value exist 
+            // 1) Swap node with successor that has the lower priority until node is a leaf
+            // 2) Delete node
+            else
+            {
+                // 1)
+                // Search vor delete node and pre
+                var (_, pre, node, dir, _) = EvenMoreDetailedSearch(value);
+                
+                // DownHeap
+                var (deletePre, deleteNode, direction) = DownHeap(pre, node, dir);
+
+                // 2)
+                var (_, _) = Delete(deletePre, deleteNode, direction);
+                return true;
+            }
+
         }
 
-        public void RotationHeap(BinSearchTreeNode node)
+
+        public (TreapNode, TreapNode, Direction) DownHeap(TreapNode preNode, TreapNode node, Direction dir)
         {
-            throw new NotImplementedException();
+            TreapNode temporary;
+
+            // Case 1: node is already a leaf => nothing needs to be done
+            if(node.Type == BinSearchTreeNode.NodeType.Leaf)
+                return (preNode, node, dir);
+
+            // Case 2: swap node with its left successor if
+            else if(node.Type == BinSearchTreeNode.NodeType.OneChild && node.Left != null                               // it is the only successor 
+                    || node.Type == BinSearchTreeNode.NodeType.Symmetric && node.Left.Priority <= node.Right.Priority   // it has a lower priority than the right one
+                    )
+            {
+                temporary = node.Left;
+                node.Left = node;
+                node = temporary;
+
+                return DownHeap(node, node.Left, Direction.Left);
+            }
+
+            // Case 3: swap node with its right successor
+            else
+            {
+                temporary = node.Right;
+                node.Right = node;
+                node = temporary;
+
+                return DownHeap(node, node.Right, Direction.Right);
+            }
+
         }
 
+        
         protected override string IntendPrint(BinSearchTreeNode node, int intend, bool endOfLine = true, Direction dir = Direction.Unset)
         {
             var n = (TreapNode) node;
             return base.IntendPrint($"{n.Value},{n.Priority}", intend, endOfLine, dir);
         }
-        internal new(TreapNode, TreapNode, TreapNode, Direction, bool) EvenMoreDetailedSearch(int value)
-        {
-            var (prePre, pre, node, dir, found) = base.EvenMoreDetailedSearch(value);
-            return ((TreapNode)prePre, (TreapNode)pre, (TreapNode)node, dir, found);
-        }
+        
     }
 }
