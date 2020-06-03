edb5e4c53d7e8a04fe4a09dcefc27a2a1e110a28
Christopher Weinert <weinertch74668@th-nuernberg.de>
2020-06-03 19:39:34 +0200

    HashTab added. Info: The hashfunction is only for testing purposes.

---
+++ AlgoDatDictionaries/Hash/HashTabSepChain.cs
@@ -0,0 +1,45 @@
+﻿using AlgoDatDictionaries.Lists;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace AlgoDatDictionaries.Hash
+{
+    class HashTabSepChain : ISet
+    {
+        public int Hashfunc(int a)          // Fest???
+        {
+            int b = (3 * a + 3) % 8;
+            return b;
+        }
+
+        ISet[] harray = new SetUnsortedLinkedList[8];
+        
+        public bool Search(int value)
+        {
+            ISet temp = harray[Hashfunc(value)];
+
+            if (temp == null)
+            {
+                return false;
+            }
+            return temp.Search(value);
+        }
+
+        public bool Insert(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
