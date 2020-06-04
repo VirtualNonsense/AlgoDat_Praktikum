0efc07cb90a8d218ace481728e61361a15315cec
Christopher Weinert <weinertch74668@th-nuernberg.de>
2020-06-04 20:49:24 +0200

    Stuff

--- AlgoDatDictionaries/Hash/HashTabQuadProb.cs
+++ AlgoDatDictionaries/Hash/HashTabQuadProb.cs
@@ -1,10 +1,75 @@
-﻿using System;
+﻿using AlgoDatDictionaries.Arrays;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AlgoDatDictionaries.Hash
 {
-    class HashTabQuadProb
+    class HashTabQuadProb : ISet
     {
+        static int k = 1;
+
+        public int Hashfunc(int a, int tries)
+        { 
+            return (a + tries^2) % (4 * k + 3);
+        }
+
+        int[] arr = new int[4 * k + 3];
+
+        public bool Search(int value)
+        {
+            return search(value).Item1;
+        }
+
+        private (bool, int) search(int value)
+        {
+            int tries = 0;
+            int place = Hashfunc(value, tries);
+
+            while (arr[place] != 0 || arr[place] == value)
+            {
+                tries++;
+                Hashfunc(value, tries);
+            }
+
+            if (arr[place] == 0)
+            {
+                return (false, place);
+            }
+
+            return (true, place);
+        }
+
+        public bool Insert(int value)
+        {
+            (bool, int) findtup = search(value);
+
+            if (findtup.Item1 == false)
+            {
+                arr[findtup.Item2] = value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Delete(int value)
+        {
+            (bool, int) findtup = search(value);
+
+            if (findtup.Item1 == true)
+            {
+
+            }
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+
     }
 }
---
+++ tests/Hash/HashTabSepChain.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AlgoDatDictionaries.Hash;
+namespace Tests.Hash
+{
+    [TestClass]
+
+    class HashTabSepChain
+    {
+        //HashTabSepChain a = new HashTabSepChain();
+        //a.Insert(12);
+        //    a.Insert(56);
+        //    a.Insert(47);
+        //    a.Insert(19);
+        //    a.Insert(89);
+        //    a.Insert(31);
+        //    a.Insert(24);
+        //    a.Insert(79);
+
+        //    a.Print();
+
+        //    Console.WriteLine();
+        //    Console.WriteLine(a.Search(12));
+        //    Console.WriteLine(a.Search(56));
+        //    Console.WriteLine(a.Search(47));
+        //    Console.WriteLine(a.Search(19));
+        //    Console.WriteLine(a.Search(89));
+        //    Console.WriteLine(a.Search(31));
+        //    Console.WriteLine(a.Search(24));
+        //    Console.WriteLine(a.Search(79));
+        //    Console.WriteLine();
+        //    Console.WriteLine(a.Search(100));
+        //    Console.WriteLine();
+        //    Console.WriteLine(a.Insert(12));
+        //    Console.WriteLine(a.Insert(24));
+        //    Console.WriteLine(a.Insert(79));
+        //    Console.WriteLine(a.Insert(47));
+
+        //    a.Delete(79);
+        //    Console.WriteLine();
+        //    a.Print();
+
+        //    a.Delete(56);
+        //    a.Delete(12);
+        //    a.Delete(31);
+        //    Console.WriteLine();
+        //    a.Print();
+
+    }
+}
