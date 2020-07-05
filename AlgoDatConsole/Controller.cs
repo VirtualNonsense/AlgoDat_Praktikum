3fc8129d2843282947b6878ccf595c759c7df344
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-07-05 20:35:59 +0200

    Added an abstraction layer to menu control input. it's not as elegant as i'd like but i supose it'll do the job

---
+++ AlgoDatConsole/Controller.cs
@@ -0,0 +1,58 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoDatConsole
+{
+    public enum Control
+    {
+        Up,
+        Down,
+        Enter,
+        Escape,
+        Unknown
+    }
+    
+
+    public class Controller
+    {
+        private readonly Dictionary<ConsoleKey, Control> _keymap;
+
+        public Controller(Dictionary<ConsoleKey, Control> keymap)
+        {
+            _keymap = keymap;
+        }
+
+        public Controller()
+        {
+            _keymap = new Dictionary<ConsoleKey, Control>()
+            {
+                {ConsoleKey.UpArrow, Control.Up},
+                {ConsoleKey.DownArrow, Control.Down},
+                {ConsoleKey.Enter, Control.Enter},
+                {ConsoleKey.Backspace, Control.Escape},
+                {ConsoleKey.Escape, Control.Escape}
+            };
+        }
+        
+        public Control AwaitInput()
+        {
+            try
+            {
+                return _keymap[Console.ReadKey().Key];
+            }
+            catch (KeyNotFoundException e)
+            {
+                return Control.Unknown;
+            }
+        }
+
+        public string GetControls()
+        {
+            return _keymap.Aggregate("", (current, kvp) 
+                => current + $"{kvp.Key} is assigned to {kvp.Value}\n");
+        }
+    }
+}
