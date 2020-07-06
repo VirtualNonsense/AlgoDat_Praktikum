18fe17bf02cf5acd13c8bf162dab91d57935dc9e
Andreas Nachtmann <virtualnonsense@mailbox.org>
2020-07-06 12:37:08 +0200

    renamed shutdown to exit, moved back/exit to bottom of menu

--- AlgoDatConsole/Demonstrator.cs
+++ AlgoDatConsole/Demonstrator.cs
@@ -11,7 +11,7 @@ namespace AlgoDatConsole
 {
     public enum MenuState
     {
-        ShutDown,
+        Exit,
         InterfaceSelection,
         
         MultiSet,
@@ -62,88 +62,90 @@ namespace AlgoDatConsole
         public Demonstrator(Controller controller)
         {
             _controller = controller;
-            _machine = new EzStateMachine<MenuTrigger, MenuState>(MenuState.InterfaceSelection, MenuState.ShutDown);
+            _machine = new EzStateMachine<MenuTrigger, MenuState>(MenuState.InterfaceSelection, MenuState.Exit);
             _ticket = _machine.Subscribe(this);
 
             #region StateMachineConfig
-            _machine.Permit(MenuTrigger.Back, MenuState.InterfaceSelection, MenuState.ShutDown);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.InterfaceSelection, MenuState.MultiSet);
             _machine.Permit(MenuTrigger.Selection1, MenuState.InterfaceSelection, MenuState.MultiSetSorted);
             _machine.Permit(MenuTrigger.Selection2, MenuState.InterfaceSelection, MenuState.Set);
             _machine.Permit(MenuTrigger.Selection3, MenuState.InterfaceSelection, MenuState.SetSorted);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.MultiSet, MenuState.InterfaceSelection);
+            _machine.Permit(MenuTrigger.Back, MenuState.InterfaceSelection, MenuState.Exit);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.MultiSet, MenuState.MultiSetUnsortedArray);
             _machine.Permit(MenuTrigger.Selection1, MenuState.MultiSet, MenuState.MultiSetUnsortedLinkedList);
+            _machine.Permit(MenuTrigger.Back, MenuState.MultiSet, MenuState.InterfaceSelection);
 
-            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetSorted, MenuState.InterfaceSelection);
             _machine.Permit(MenuTrigger.Selection0, MenuState.MultiSetSorted, MenuState.MultiSetSortedArray);
             _machine.Permit(MenuTrigger.Selection1, MenuState.MultiSetSorted, MenuState.MultiSetSortedLinkedList);
+            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetSorted, MenuState.InterfaceSelection);
 
-            _machine.Permit(MenuTrigger.Back, MenuState.Set, MenuState.InterfaceSelection);
             _machine.Permit(MenuTrigger.Selection0, MenuState.Set, MenuState.SetUnsortedArray);
             _machine.Permit(MenuTrigger.Selection1, MenuState.Set, MenuState.SetUnsortedLinkedList);
             _machine.Permit(MenuTrigger.Selection2, MenuState.Set, MenuState.HashTabQuadProb);
             _machine.Permit(MenuTrigger.Selection3, MenuState.Set, MenuState.HashTabSepChain);
+            _machine.Permit(MenuTrigger.Back, MenuState.Set, MenuState.InterfaceSelection);
 
-            _machine.Permit(MenuTrigger.Back, MenuState.SetSorted, MenuState.InterfaceSelection);
             _machine.Permit(MenuTrigger.Selection0, MenuState.SetSorted, MenuState.SetSortedArray);
             _machine.Permit(MenuTrigger.Selection1, MenuState.SetSorted, MenuState.SetSortedLinkedList);
             _machine.Permit(MenuTrigger.Selection2, MenuState.SetSorted, MenuState.BinSearchTree);
             _machine.Permit(MenuTrigger.Selection3, MenuState.SetSorted, MenuState.AVLTree);
             _machine.Permit(MenuTrigger.Selection4, MenuState.SetSorted, MenuState.Treap);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetUnsortedArray, MenuState.MultiSet);
+            _machine.Permit(MenuTrigger.Back, MenuState.SetSorted, MenuState.InterfaceSelection);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.MultiSetUnsortedArray, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.MultiSetUnsortedArray, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetUnsortedLinkedList, MenuState.MultiSet);
+            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetUnsortedArray, MenuState.MultiSet);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.MultiSetUnsortedLinkedList, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.MultiSetUnsortedLinkedList, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetSortedArray, MenuState.MultiSetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetUnsortedLinkedList, MenuState.MultiSet);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.MultiSetSortedArray, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.MultiSetSortedArray, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetSortedLinkedList, MenuState.MultiSetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetSortedArray, MenuState.MultiSetSorted);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.MultiSetSortedLinkedList, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.MultiSetSortedLinkedList, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.SetUnsortedArray, MenuState.Set);
+            _machine.Permit(MenuTrigger.Back, MenuState.MultiSetSortedLinkedList, MenuState.MultiSetSorted);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.SetUnsortedArray, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.SetUnsortedArray, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.SetUnsortedLinkedList, MenuState.Set);
+            _machine.Permit(MenuTrigger.Back, MenuState.SetUnsortedArray, MenuState.Set);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.SetUnsortedLinkedList, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.SetUnsortedLinkedList, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.HashTabQuadProb, MenuState.Set);
+            _machine.Permit(MenuTrigger.Back, MenuState.SetUnsortedLinkedList, MenuState.Set);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.HashTabQuadProb, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.HashTabQuadProb, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.HashTabSepChain, MenuState.Set);
+            _machine.Permit(MenuTrigger.Back, MenuState.HashTabQuadProb, MenuState.Set);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.HashTabSepChain, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.HashTabSepChain, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.SetSortedArray, MenuState.SetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.HashTabSepChain, MenuState.Set);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.SetSortedArray, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.SetSortedArray, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.SetSortedLinkedList, MenuState.SetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.SetSortedArray, MenuState.SetSorted);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.SetSortedLinkedList, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.SetSortedLinkedList, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.BinSearchTree, MenuState.SetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.SetSortedLinkedList, MenuState.SetSorted);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.BinSearchTree, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.BinSearchTree, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.AVLTree, MenuState.SetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.BinSearchTree, MenuState.SetSorted);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.AVLTree, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.AVLTree, MenuState.Insert);
-            
-            _machine.Permit(MenuTrigger.Back, MenuState.Treap, MenuState.SetSorted);
+            _machine.Permit(MenuTrigger.Back, MenuState.AVLTree, MenuState.SetSorted);
+
             _machine.Permit(MenuTrigger.Selection0, MenuState.Treap, MenuState.Delete);
             _machine.Permit(MenuTrigger.Selection1, MenuState.Treap, MenuState.Insert);
+            _machine.Permit(MenuTrigger.Back, MenuState.Treap, MenuState.SetSorted);
+
             #endregion
         }
 
@@ -162,8 +164,7 @@ namespace AlgoDatConsole
         
         public void OnCompleted()
         {
-            Console.WriteLine("Demonstrator shutdown");
-            _ticket.Dispose();
+            Console.WriteLine("Exiting Demonstrator");
         }
 
         public void OnError(Exception error)
@@ -178,7 +179,7 @@ namespace AlgoDatConsole
             Console.Clear();
             switch (s)
             {
-                case MenuState.ShutDown:
+                case MenuState.Exit:
                     break;
                 case MenuState.InterfaceSelection:
                     _machine.Trigger(GetStateTransition(null));
