b92455b5605727d93be803f5309232e4572b5543
Hedtt <32038803+Hedtt@users.noreply.github.com>
2020-07-06 15:00:16 +0200

    fixed insert after delete

--- AlgoDatDictionaries/Hash/HashTabQuadProb.cs
+++ AlgoDatDictionaries/Hash/HashTabQuadProb.cs
@@ -18,13 +18,13 @@ namespace AlgoDatDictionaries.Hash
             }
         }
 
-        private int HashfuncPos(int a, int tries) 
+        public int HashfuncPos(int a, int tries) 
         { 
             return (a + tries*tries) % (4 * k + 3);
             
         }
 
-        private int HashfuncNeg(int a, int tries)
+        public int HashfuncNeg(int a, int tries)
         {
             return ((a - tries * tries) % (4 * k + 3) + (4* k +3))%(4*k+3); //avoiding negative
         }
@@ -35,41 +35,55 @@ namespace AlgoDatDictionaries.Hash
         {
             return search(value).Item1;
         }
-        
-        
-       
 
         private (bool, int) search(int value)
         {
-            
             int tries = 0;
-            double max = (4 * k + 3) / 2; 
+            double max = (4 * k + 3) / 2; //Math.Floor in ugly
+            int thinker = -1;
+            int hashfuncpos = HashfuncPos(value, tries);
+            int hashfuncneg = HashfuncNeg(value, tries);
 
-            while (tries <= Math.Floor(max))
+            while (tries <= Math.Floor(max))          
             {
-                var hashfuncpos = HashfuncPos(value, tries);
-                var hashfuncneg = HashfuncNeg(value, tries);
-                if (arr[hashfuncpos] == value)    //checking if calculated position contains value
+                if (arr[hashfuncpos] == value)    //value was found
                 {
                     return (true, hashfuncpos);
                 }
-
                 if (arr[hashfuncneg] == value)
                 {
                     return (true, hashfuncneg);
                 }
-
-                if (arr[hashfuncpos] == -1) //if -1 is found, no cell before could contain value, so it's not in array
+                if (arr[hashfuncpos] == -1 && arr[hashfuncneg] == -1 && thinker == -1 ) // if -1 is found, the value is not in the array, returning first free position
                 {
-                    return (false, hashfuncpos); //returning the empty position for runtime optimization
+                    return (false, hashfuncpos);
                 }
 
-                if (arr[hashfuncneg] == -1)
+                if (arr[hashfuncpos] != -1 && arr[hashfuncneg] == -1 && thinker == -1)
                 {
                     return (false, hashfuncneg);
                 }
+                
+                if ((arr[hashfuncpos] == -1 && arr[hashfuncneg] == -1 ||     //value is not in array, but cell previous to current
+                     arr[hashfuncpos] != -1 && arr[hashfuncneg] == -1) 
+                    && thinker != -1)
+                {
+                    return (false, thinker);
+                }
 
+
+                if (arr[hashfuncpos] == -2 && thinker == -1)    //cell contained another value before, value could be after, but remembering cell for insert
+                {
+                    thinker = hashfuncpos;
+                }
+
+                if (arr[hashfuncneg] == -2 && thinker == -1)
+                {
+                    thinker = hashfuncneg;
+                }
                 tries++;
+                hashfuncpos = HashfuncPos(value, tries);
+                hashfuncneg = HashfuncNeg(value, tries);
             }
 
             return (false, -1);                 // Instead of -1 "thinker" possible
@@ -91,10 +105,9 @@ namespace AlgoDatDictionaries.Hash
         public bool Delete(int value)
         {
             (bool, int) findtup = search(value);
-
-            if(findtup.Item1)
+            if(findtup.Item1 == true)
             {
-                arr[findtup.Item2] = -2;                             // delete item and set cell to used (-2)
+                arr[findtup.Item2] = -2;                 
                 return true;
             }
 
@@ -106,17 +119,13 @@ namespace AlgoDatDictionaries.Hash
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write($"{i}) ");
-                switch (arr[i])
+                if (arr[i] == -1 || arr[i] == -2)                                  
                 {
-                    case -1:
-                        Console.WriteLine("Slot is empty");
-                        break;
-                    case -2:
-                        Console.WriteLine("Slot is empty, but blocked");
-                        break;
-                    default:
-                        Console.WriteLine($"{arr[i]} ");
-                        break;
+                    Console.WriteLine("Slot is empty");
+                }
+                else
+                {
+                    Console.WriteLine($"{arr[i]} ");
                 }
             }
         }
